18953c2bf612f909e8a80ea2f77cb19db7c79519
ondrejstasek <61665409+ondrejstasek@users.noreply.github.com>
2024-12-05 23:14:22 +0100

    fireworks on document

--- grocy-chores-card.js
+++ grocy-chores-card.js
@@ -715,7 +715,7 @@ class GrocyChoresCard extends LitElement {
             });
         }
         this._fireHaptic();
-        new Fireworks(document.querySelector('hui-sections-view')).launch(Math.floor(Math.random() * 10) + 5)
+        new Fireworks(document).launch(Math.floor(Math.random() * 10) + 5)
     }
     
     _fireHaptic() {
